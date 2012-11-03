5860fd225eda71bbfb36b9ae9339866ec19d5041
Diarmuid Wrenne <diarmuid@bluekulu.com>
2012-11-03 11:55:30 +0000

    added in gondor specific files and settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -97,6 +97,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.contrib.messages.context_processors.messages",
     "pinax_utils.context_processors.settings",
     "account.context_processors.account",
+    "{{ project_name }}.context_processors.settings",
 ]
 
 
